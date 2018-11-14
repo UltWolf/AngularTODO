02520d3d193ae80081405e33e34986614313a7f2
UltWolf <ultwolf@gmail.com>
2018-11-14 23:39:08 +0200

    Add column in the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColumnComponent } from './column/column.component';
+import { ColumnControllerComponent } from './column-controller/column-controller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColumnComponent,
+    ColumnControllerComponent
   ],
   imports: [
     BrowserModule,

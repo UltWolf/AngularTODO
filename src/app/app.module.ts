a1e5ac4cd7d8c930515f1f6b05a5b23dfdc90be7
Denis Zubritskiy <katatsu12@gmail.com>
2018-11-14 23:36:36 +0200

    Initial dashboard features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardsComponent } from './dashboards/dashboards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardsComponent
   ],
   imports: [
     BrowserModule,
